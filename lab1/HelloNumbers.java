809f0f1c04717f33c5edfc27d157639347ef5cc7
paul <872165606@qq.com>
2024-12-27 14:51:35 +0800

    Finish Lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,8 @@
+public class HelloNumbers {
+    public static void main(string[] args){
+        int x = 0;
+        while (x < 10){
+            System.out.print(x + "");
+        }
+    }
+}
--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,12 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Yingte Dai
  */
 public class LeapYear {
+
+    /** Return true if the given year is a leap year */
+    private static boolean isLeapYear(int year){
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
